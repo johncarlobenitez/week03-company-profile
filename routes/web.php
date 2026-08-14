14211d5d71ac2f2d9ec09ef80b9ecb24b88e586d
johncarlobenitez <johncarlobenitez831@gmail.com>
2026-08-14 22:19:06 +0800

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,26 @@
 use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
 
-Route::get('/', [CompanyController::class, 'home'])->name('company');
+// Welcome / Landing Page
+Route::get('/', [CompanyController::class, 'welcome'])->name('welcome');
 
+// Main Company Homepage
+Route::get('/company', [CompanyController::class, 'home'])->name('company');
+
+// About
 Route::get('/about', [CompanyController::class, 'about'])->name('about');
 
+// Services
 Route::get('/services', [CompanyController::class, 'services'])->name('services');
 
+// Contact
 Route::get('/contact', [CompanyController::class, 'contact'])->name('contact');
+
+// Contact Form Submission
 Route::post('/contact', [CompanyController::class, 'store'])->name('contact.store');
