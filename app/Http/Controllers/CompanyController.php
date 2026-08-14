2916e5588a77640f45399e596f62f1d44ec24829
johncarlobenitez <johncarlobenitez831@gmail.com>
2026-08-14 22:27:08 +0800

    fix welcome view path in controller

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -8,26 +8,49 @@ use Illuminate\View\View;
 
 class CompanyController extends Controller
 {
+    /**
+     * Welcome / Landing Page
+     */
+    public function welcome(): View
+    {
+        return view('pages.welcome');
+    }
+
+    /**
+     * Main Company Homepage
+     */
     public function home(): View
     {
         return view('pages.home');
     }
 
+    /**
+     * About Page
+     */
     public function about(): View
     {
         return view('pages.about');
     }
 
+    /**
+     * Services Page
+     */
     public function services(): View
     {
         return view('pages.services');
     }
 
+    /**
+     * Contact Page
+     */
     public function contact(): View
     {
         return view('pages.contact');
     }
 
+    /**
+     * Handle Contact Form Submission
+     */
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
@@ -36,6 +59,8 @@ class CompanyController extends Controller
             'message' => ['required', 'string', 'min:10'],
         ]);
 
-        return redirect()->route('contact')->with('status', 'Thank you! We\'ll be in touch soon.');
+        return redirect()
+            ->route('contact')
+            ->with('status', 'Thank you! We\'ll be in touch soon.');
     }
 }
